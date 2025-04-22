2fae640c5ee095e7d761ddb8144eae92f23bfab7
jhspetersson <jhspetersson@gmail.com>
2025-04-22 12:10:25 +0200

    add tests for zipping gatherers

--- lib/src/test/java/io/github/jhspetersson/packrat/ZipTest.java
+++ lib/src/test/java/io/github/jhspetersson/packrat/ZipTest.java
@@ -2,6 +2,7 @@ package io.github.jhspetersson.packrat;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -47,6 +48,18 @@ public class ZipTest {
     }
 
     @Test
+    public void zipStreamToMapTest() {
+        var names = List.of("Anna", "Mike", "Sandra", "Rudolf", "Monica");
+        var ages = Stream.of(20, 30, 40, 50, 60, 70, 80, 90);
+
+        var users = names.stream().gather(Packrat.zip(ages)).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        assertEquals(names.size(), users.size());
+        assertEquals(20, users.get("Anna"));
+        assertEquals(60, users.get("Monica"));
+    }
+
+    @Test
     public void zipLongerWithShorterTest() {
         var names = List.of("Anna", "Mike", "Sandra", "Rudolf", "Monica");
         var ages = List.of(20, 30, 40);
@@ -59,4 +72,30 @@ public class ZipTest {
     }
 
     record User(String name, int age) {}
+
+    @Test
+    public void zipWithIteratorTest() {
+        var names = List.of("Anna", "Mike", "Sandra", "Rudolf", "Monica");
+        var ages = List.of(20, 30, 40, 50, 60, 70, 80, 90);
+        Iterator<Integer> agesIterator = ages.iterator();
+
+        var users = names.stream().gather(Packrat.zip(agesIterator)).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        assertEquals(names.size(), users.size());
+        assertEquals(20, users.get("Anna"));
+        assertEquals(60, users.get("Monica"));
+    }
+
+    @Test
+    public void zipWithIteratorAndMapperTest() {
+        var names = List.of("Anna", "Mike", "Sandra", "Rudolf", "Monica");
+        var ages = List.of(20, 30, 40, 50, 60, 70, 80, 90);
+        Iterator<Integer> agesIterator = ages.iterator();
+
+        var users = names.stream().gather(Packrat.zip(agesIterator, User::new)).toList();
+
+        assertEquals(names.size(), users.size());
+        assertEquals(new User("Anna", 20), users.getFirst());
+        assertEquals(new User("Monica", 60), users.getLast());
+    }
 }
