ce9716f78d641924f59990be3c1feaf439b62e7a
jhspetersson <jhspetersson@gmail.com>
2026-03-13 17:29:17 +0100

    move ZipGatherer iterator into state object

--- src/main/java/io/github/jhspetersson/packrat/ZipGatherer.java
+++ src/main/java/io/github/jhspetersson/packrat/ZipGatherer.java
@@ -3,6 +3,7 @@ package io.github.jhspetersson.packrat;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 import java.util.stream.Gatherer;
 import java.util.stream.Stream;
 
@@ -16,13 +17,17 @@ import org.jspecify.annotations.NonNull;
  * @param <V> mapped element type
  * @author jhspetersson
  */
-class ZipGatherer<T, U, V> implements Gatherer<T, Void, V> {
-    private final Iterator<? extends U> iterator;
+class ZipGatherer<T, U, V> implements Gatherer<T, ZipGatherer.State<U>, V> {
+    private final Supplier<Iterator<? extends U>> iteratorSupplier;
     private final BiFunction<? super T, ? super U, ? extends V> mapper;
 
     ZipGatherer(@NonNull Iterable<? extends U> input,
                 @NonNull BiFunction<? super T, ? super U, ? extends V> mapper) {
-        this(input.iterator(), mapper);
+        Objects.requireNonNull(input, "input cannot be null");
+        Objects.requireNonNull(mapper, "mapper cannot be null");
+
+        this.iteratorSupplier = input::iterator;
+        this.mapper = mapper;
     }
 
     ZipGatherer(@NonNull Stream<? extends U> input,
@@ -35,18 +40,31 @@ class ZipGatherer<T, U, V> implements Gatherer<T, Void, V> {
         Objects.requireNonNull(iterator, "iterator cannot be null");
         Objects.requireNonNull(mapper, "mapper cannot be null");
 
-        this.iterator = iterator;
+        this.iteratorSupplier = () -> iterator;
         this.mapper = mapper;
     }
 
     @Override
-    public Integrator<Void, T, V> integrator() {
-        return Integrator.ofGreedy((_, element, downstream) -> {
-            if (iterator.hasNext()) {
-                var mappedValue = mapper.apply(element, iterator.next());
+    public Supplier<State<U>> initializer() {
+        return () -> new State<>(iteratorSupplier.get());
+    }
+
+    @Override
+    public Integrator<State<U>, T, V> integrator() {
+        return Integrator.ofGreedy((state, element, downstream) -> {
+            if (state.iterator.hasNext()) {
+                var mappedValue = mapper.apply(element, state.iterator.next());
                 return downstream.push(mappedValue);
             }
             return false;
         });
+    }
+
+    static class State<U> {
+        final Iterator<? extends U> iterator;
+
+        State(Iterator<? extends U> iterator) {
+            this.iterator = iterator;
+        }
     }
 }
