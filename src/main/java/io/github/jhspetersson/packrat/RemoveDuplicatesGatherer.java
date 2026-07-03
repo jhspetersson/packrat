662857de2ad8de0b8f851925f81c76b5577d55c4
jhspetersson <jhspetersson@gmail.com>
2026-07-03 19:56:13 +0200

    simplify removeDuplicates state

--- src/main/java/io/github/jhspetersson/packrat/RemoveDuplicatesGatherer.java
+++ src/main/java/io/github/jhspetersson/packrat/RemoveDuplicatesGatherer.java
@@ -1,7 +1,5 @@
 package io.github.jhspetersson.packrat;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -13,12 +11,12 @@ import org.jspecify.annotations.NonNull;
  * Removes consecutive duplicates from a stream based on a mapping function.
  * Only adjacent elements that have equal mapped values will be considered duplicates.
  * If no mapping function is provided, elements are compared directly.
- * 
+ *
  * @param <T> element type
  * @param <U> mapped element type
  * @author jhspetersson
  */
-class RemoveDuplicatesGatherer<T, U> implements Gatherer<T, List<U>, T> {
+class RemoveDuplicatesGatherer<T, U> implements Gatherer<T, RemoveDuplicatesGatherer.State<U>, T> {
     private final Function<? super T, ? extends U> mapper;
 
     RemoveDuplicatesGatherer(@NonNull Function<? super T, ? extends U> mapper) {
@@ -27,21 +25,26 @@ class RemoveDuplicatesGatherer<T, U> implements Gatherer<T, List<U>, T> {
     }
 
     @Override
-    public Supplier<List<U>> initializer() {
-        return ArrayList::new;
+    public Supplier<State<U>> initializer() {
+        return State::new;
     }
 
     @Override
-    public Integrator<List<U>, T, T> integrator() {
+    public Integrator<State<U>, T, T> integrator() {
         return Integrator.of((state, element, downstream) -> {
             var mappedValue = mapper.apply(element);
-            if (!state.isEmpty() && Objects.equals(state.getFirst(), mappedValue)) {
+            if (state.hasValue && Objects.equals(state.value, mappedValue)) {
                 return !downstream.isRejecting();
             } else {
-                state.clear();
-                state.add(mappedValue);
+                state.hasValue = true;
+                state.value = mappedValue;
                 return downstream.push(element);
             }
         });
+    }
+
+    static class State<U> {
+        boolean hasValue;
+        U value;
     }
 }
